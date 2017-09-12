2a49ae741b6649126e4f6ffa613adda3fc929a20
TalhaAsmal <talha.asmal@gmail.com>
2017-09-12 21:47:02 +0200

    Lecture 122/123

--- MyCode/ProTwo/AppTwo/views.py
+++ MyCode/ProTwo/AppTwo/views.py
@@ -4,3 +4,7 @@ from django.http import HttpResponse
 # Create your views here.
 def index(request):
     return HttpResponse("<em>Second App</em>")
+
+def help(request):
+    my_dict = {'insert_me': '<h1>Help Page</h1>'}
+    return render(request, 'AppTwo/help.html', context=my_dict)
